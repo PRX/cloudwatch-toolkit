ead0c5dab2a7c39ce4cbe66948dbbae120e11db3
Chris Kalafarski <chris@farski.com>
2024-04-21 15:22:30 -0400

    Remove unnecessary escape

--- src/alarm-slack-reminders/urls.mjs
+++ src/alarm-slack-reminders/urls.mjs
@@ -7,6 +7,6 @@
 export function alarmConsole(alarmDetail) {
   const name = alarmDetail.AlarmName;
   const region = alarmDetail.AlarmArn.split(":")[3];
-  const encoded = encodeURI(name.replace(/\ /g, "+")).replace(/%/g, "$");
+  const encoded = encodeURI(name.replace(/ /g, "+")).replace(/%/g, "$");
   return `https://console.aws.amazon.com/cloudwatch/home?region=${region}#alarmsV2:alarm/${encoded}`;
 }
